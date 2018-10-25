01daba2b097ecebfd7435dbef0cc109d8ad7f9d3
Fernan de Dios <info@mediagramstudios.com>
2018-10-26 06:51:29 +0800

    add topic model

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,9 +2,9 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   def new(conn, params) do
-    IO.puts "++++"
-    IO.inspect conn
-    IO.puts "++++"
-    IO.inspect params
+    # IO.inspect conn
+    # IO.inspect params
+
+    
   end
 end

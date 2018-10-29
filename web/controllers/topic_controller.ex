1fbc2d4a863d70a4c84831fe7819ce5a91a6d241
Fernan de Dios <info@mediagramstudios.com>
2018-10-29 12:30:38 +0800

    topic controller, route and new template adjustments

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,7 +3,7 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # IO.inspect conn
     # IO.inspect params
 
@@ -14,5 +14,21 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset  # plucked from templates/topic folder
+  end
+
+  #def create(conn, params) do
+    # IO.inspect params
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      # {:ok, post} -> IO.inspect(post)
+      # {:error, changeset} -> IO.inspect(changeset)
+
+      {:ok, post} ->
+      {:error, changeset} -> IO.inspect(post)
+        render conn, "new.html", changeset: changeset
+    end
   end
 end

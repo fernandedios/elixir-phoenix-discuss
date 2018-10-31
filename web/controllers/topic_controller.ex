84d349e084d221273bc720dd8d11d58ee90af066
Fernan de Dios <info@mediagramstudios.com>
2018-11-01 05:24:49 +0800

    added edit function on  topic controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -37,7 +37,14 @@ defmodule Discuss.TopicController do
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
     end
+  end
 
+  # get id from params
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id) # get single record with id topic_id
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
   end
 
 end

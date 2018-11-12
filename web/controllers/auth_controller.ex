ecb2824f6a162deaf0117ba03c76406c9caf451f
Fernan de Dios <info@mediagramstudios.com>
2018-11-13 04:07:28 +0800

    fix auth_controller typo

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,9 +1,9 @@
 defmodule Discuss.AuthController do
-  use Discuss.web, :controller
+  use Discuss.Web, :controller
   plug Ueberauth
 
   def callback(conn, params) do
     IO.inspect(conn.assigns)
-    IO.inspect(params) 
+    IO.inspect(params)
   end
 end

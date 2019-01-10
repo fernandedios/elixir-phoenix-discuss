4f29c914e4db22f81230bf503905e018fdc0748f
Fernan de Dios <info@mediagramstudios.com>
2019-01-11 04:55:05 +0800

    added check_topic_owner plug

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -7,6 +7,9 @@ defmodule Discuss.TopicController do
   # only executes for the given routes in guard clause
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
 
+  # function plug
+  plug :check_topic_owner when action in [:update, :edit, :delete]
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
@@ -79,6 +82,20 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  # _params is NOT from form or router
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn # return conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that") # display error
+      |> redirect(to: topic_path(conn, :index)) # redirect to index
+      |> halt() # stop further processing
+    end
   end
 
 end

1a4720f2fbf2cf96b16619f194e91a07db5eec32
Fernan de Dios <info@mediagramstudios.com>
2019-01-23 04:41:36 +0800

    updates to comments html

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,10 +5,16 @@ socket.connect();
 
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {});
-  
+
   channel.join()
     .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) });
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { content: content });
+  });
 }
 
 window.createSocket = createSocket;

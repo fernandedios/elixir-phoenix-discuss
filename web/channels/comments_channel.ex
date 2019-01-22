72b74868198e923ed7e3f2bb90c9ce8fe61f0f02
Fernan de Dios <info@mediagramstudios.com>
2019-01-23 04:33:59 +0800

    updates to commentsChannel

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,14 +1,19 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join(name, _params, socket) do
-    {:ok, %{hey: "there"}, socket}
+  alias Discuss.Topic
+
+  # "comments:" <> topic_id
+  # pull id and assign as topic_id
+  # topic_id is a string
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id) # convert to int
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
   def handle_in(name, message, socket) do
-    IO.puts(name)
-    IO.inspect(message)
-
     {:reply, :ok, socket}
   end
 end
